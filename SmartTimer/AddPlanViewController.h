aa84e0661b01720ef71cb8c32c33dd7ebdae8054
David bao <davidbao@netease.com>
2014-02-20 22:43:39 +0800

    Add some common files and BlueShield files.

---
+++ SmartTimer/AddPlanViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddPlanViewController.h
+//  SmartTimer
+//
+//  Created by baowei on 14-2-20.
+//  Copyright (c) 2014年 TicktockLib. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddPlanViewController : UITableViewController
+
+@end
